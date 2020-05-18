f7ccef7c001484c57f62f3948ec77f0e1585582b
mysterio42 <bachana3435@gmail.com>
2020-05-18 17:34:30 +0400

    change the mind

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from models.net import FeedForward
 from models.embedding import WikipediaEmbedding
 
 if __name__ == '__main__':
-    wk_emb = WikipediaEmbedding(vocab_size=20000)
+    wk_emb = WikipediaEmbedding(vocab_size=1000)
     wk_emb.build()
     model = FeedForward(emb=wk_emb)
     model.train()
