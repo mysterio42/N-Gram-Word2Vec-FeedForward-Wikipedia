ebac59150eafee1e399cac09c5c8e4a7de92cb55
mysterio42 <bachana3435@gmail.com>
2020-05-19 15:05:16 +0400

    json to csv

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from models.net import FeedForward
 from models.embedding import WikipediaEmbedding
+from utils.data import to_csv
 from utils.model import load_model, load_embedding,top_neighs,analogy
 import argparse
 
@@ -34,10 +35,13 @@ if __name__ == '__main__':
         word2idx = load_embedding()
         idx2word = {i: w for w, i in word2idx.items()}
         for We in (W, (W + V.T) / 2):
-            neighs = top_neighs('population', word2idx, idx2word, We)
+            neighs = top_neighs('king', word2idx, idx2word, We)
             solution = analogy('world', 'city', 'population', word2idx, idx2word, We)
             print(solution)
             print(neighs)
+            to_csv('neighs','king',neighs)
+
+
     else:
         wk_emb = WikipediaEmbedding(vocab_size=1000)
         wk_emb.build()
